9e78f8dbff4bce5c0bee8c190a2ef01b83c7683d
Alexandre Brilhante <alexandre.brilhante@gmail.com>
2018-11-09 20:04:42 -0500

    fixed typos

--- r/fibonacci.r
+++ r/fibonacci.r
@@ -1,8 +1,8 @@
 fibonacci <- function(n) {
-    ifelse(n < 2, n, fibonacci(n - 1) + fifibonaccib(n - 2))
+    ifelse(n < 2, n, fibonacci(n - 1) + fibonacci(n - 2))
 }
 
 start <- Sys.time()
 
-print(fib(25))
+print(fibonacci(25))
 print(Sys.time() - start)
